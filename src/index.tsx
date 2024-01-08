ae64f3e5b5a02d301262495dfe1b486004b2b743
Deokti <deonkens@gmail.com>
2024-01-08 19:55:52 +0300

    refactor: mode file App in app/App, rename main.tsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app/App/App";
 
 import "@fontsource/roboto";
 import "./app/styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
