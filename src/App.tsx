88dd83bd93d8ebd7ae82040ebb5b647d659b2762
Deokti <deonkens@gmail.com>
2024-01-08 19:49:20 +0300

    chore: add favicons, manifest and description project in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
 
 function App() {
@@ -8,25 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a target="_blank">
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
       {/* eslint-disable-next-line i18next/no-literal-string */}
       <h1>Vite + React</h1>
       <div className="card">
