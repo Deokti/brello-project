4c0c44d239d119dfa597c2bf45d111189966a9b2
Deokti <deonkens@gmail.com>
2024-01-08 19:20:13 +0300

    chore(linting): add Husky (pre-commit-hooks)

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,18 @@ function App() {
           />
         </a>
       </div>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <h1>Vite + React</h1>
       <div className="card">
+        {/* eslint-disable-next-line i18next/no-literal-string */}
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
+          {/* eslint-disable-next-line i18next/no-literal-string */}
+          {/* eslint-disable-next-line i18next/no-literal-string */}
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
