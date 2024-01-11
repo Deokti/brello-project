7c876add0ce7025f75cb4084a97f498a326258c0
Daniil Kochkin <deokneks@gmail.com>
2024-01-11 14:04:53 +0300

    Update vite.config.ts
    Co-authored-by: Sova <5620073+sergeysova@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
+      "~/": path.resolve(__dirname, "./src"),
     },
   },
 });
