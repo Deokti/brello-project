f4465b06e5151b9ddf6a727283ad9981e863b2d5
daniilkochkin <deokneks@gmail.com>
2024-02-04 19:44:34 +0300

    feat: create shared ui Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,27 @@
-import { useTranslation } from "react-i18next";
-
 import "@fontsource/roboto";
 import "@/shared/config/i18n/i18n";
 import "./styles/index.scss";
+import SocialIcon from "./social-icon.svg?react";
+
+import { Button } from "@/shared/ui/Button";
 
 export function App() {
-  const { t, i18n } = useTranslation("common");
-
-  const onSelectedLang = async (lang: string) => {
-    await i18n.changeLanguage(lang);
-  };
-
   return (
-    <>
-      <ul style={{ display: "flex", gap: 10, justifyContent: "center", padding: 0 }}>
-        <li onClick={() => onSelectedLang("ru")}>
-          <button>RU</button>
-        </li>
-        <li onClick={() => onSelectedLang("en")}>
-          <button>EN</button>
-        </li>
-      </ul>
-      <div style={{ marginTop: 20 }}>
-        <h1>{t("title")}</h1>
-        <p>{t("description")}</p>
-      </div>
-    </>
+    <div className="app main-theme">
+      <Button
+        style={{ margin: 20 }}
+        disabled
+      >
+        Get started
+      </Button>
+
+      <Button
+        leftIcon={<SocialIcon />}
+        color="secondary"
+        variant={"outlined"}
+      >
+        Sign up with Google
+      </Button>
+    </div>
   );
 }
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,52 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import cx from "classnames";
+
+import styles from "./Button.module.scss";
+
+type ButtonVariant = "contained" | "outlined";
+type ButtonColor = "primary" | "secondary" | "error";
+type ButtonSize = "sm" | "md" | "lg" | "xl" | "xl2";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  variant?: ButtonVariant;
+  color?: ButtonColor;
+  size?: ButtonSize;
+}
+
+export const Button = (props: ButtonProps) => {
+  const {
+    className,
+    fullWidth,
+    disabled,
+    children,
+    leftIcon,
+    rightIcon,
+    color = "primary",
+    variant = "contained",
+    size = "lg",
+    ...otherProps
+  } = props;
+
+  const mods = {
+    [styles.fullWidth]: fullWidth,
+  };
+
+  const additional = [styles[color], styles[variant], styles[size], className];
+
+  return (
+    <button
+      {...otherProps}
+      disabled={disabled}
+      className={cx(styles.button, mods, additional)}
+    >
+      {leftIcon && <span className={styles.icon}>{leftIcon}</span>}
+      {children}
+      {rightIcon && <span className={styles.icon}>{rightIcon}</span>}
+    </button>
+  );
+};
