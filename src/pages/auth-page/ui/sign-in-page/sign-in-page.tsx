c9b4162a33352be6a672cc2be0adcf47bcb9bd1c
daniilkochkin <deokneks@gmail.com>
2024-02-07 14:35:56 +0300

    feat: completing the desktop layout for the sign-in page

--- src/pages/auth-page/ui/sign-in-page/sign-in-page.tsx
+++ src/pages/auth-page/ui/sign-in-page/sign-in-page.tsx
@@ -1,12 +1,40 @@
-import cx from "classnames";
+import { Input } from "@/shared/ui/Input";
+import { Button } from "@/shared/ui/Button";
 
+import GoogleIcon from "../../assets/google-icon.svg?react";
 import { AuthPageLayout } from "../components/auth-page-layout";
 import styles from "./sign-in-page.module.scss";
 
 export const SignInPage = () => {
   return (
-    <div className={cx(styles.root)}>
-      <AuthPageLayout>Sign In Page</AuthPageLayout>
+    <div className={styles.root}>
+      <AuthPageLayout>
+        <div className={styles.inner}>
+          <h1 className={styles.title}>Sign In</h1>
+          <h2 className={styles.subtitle}>Start your 30-day free trial.</h2>
+
+          <Input
+            label={"Email"}
+            placeholder={"Enter your email"}
+            className={styles.input}
+          />
+
+          <Button
+            fullWidth
+            className={styles.button}
+          >
+            Get started
+          </Button>
+          <Button
+            fullWidth
+            leftIcon={<GoogleIcon />}
+            variant={"outlined"}
+            color={"secondary"}
+          >
+            Sign up with Google
+          </Button>
+        </div>
+      </AuthPageLayout>
     </div>
   );
 };
