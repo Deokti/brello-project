d24b3cbc425c70d07ddb8efa4fe7f440465fa847
Deokti <deonkens@gmail.com>
2024-03-08 17:11:08 +0300

    build: fixing the vite project build for images

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
