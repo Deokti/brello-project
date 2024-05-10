bf0bdb52b2911d9ba9501e7a42fb2828d3d30dba
Deokti <deonkens@gmail.com>
2024-05-10 14:54:22 +0300

    fix: words

--- src/pages/auth-page/model/auth.ts
+++ src/pages/auth-page/model/auth.ts
@@ -7,36 +7,35 @@ import { apiAuth } from "@/shared/lib/api/apiAuth";
 /**
  * Аутентификация через ввод email
  * 1. $email Пользователь вводит email
- * 2. Убеждаемся, что вввод email корренктный, и пользователь не ввёл тарабаршину
+ * 2. Убеждаемся, что ввод email корректный, и пользователь не ввёл тарабарщину
  *  2.1. $error. Если email не корректный показываем ошибку: "Должен быть корректный адрес электронной почты"
  *  2.2. $success Если email корректный отправляем запрос на API
- * 3. $pedding Пока запрос отправляется, пользователь не может нажимать ни на что - показывается loader
+ * 3. $pending Пока запрос отправляется, пользователь не может нажимать ни на что - показывается loader
  * 4. Когда запрос отправляем есть несколько сценариев:
  *  4.1. Если отправка завершилась успешно, перенаправляем на страницу `auth/success`
- *  4.2. $error Если запрос проволился, перенаправляем на странице `auth/error`
+ *  4.2. $error Если запрос провалился, перенаправляем на странице `auth/error`
  */
-
 
 const signInPost = attach({ effect: apiAuth.loginByEmailAndPassword });
 
 export const emailChanged = createEvent<string>("");
-export const formSubmitted = createEvent(); 
+export const formSubmitted = createEvent();
 
 export const $email = createStore<string>("");
 
-export const $pedding = createStore<boolean>(false);
+export const $pending = createStore<boolean>(false);
 export const $error = createStore<SignInPageError | null>(null);
 export const $success = createStore<boolean>(false);
 
-const $isValidEmail = $email.map((email) => vilidateEmail.test(email))
+const $isValidEmail = $email.map((email) => vilidateEmail.test(email));
 
 $email.on(emailChanged, (_, newEmailValue) => newEmailValue);
 
-// Очищание ошибки при исправлении email
+// Очищение ошибки при исправлении email
 sample({
   clock: emailChanged,
   fn: () => null,
-  target: $error
+  target: $error,
 });
 
 // Записывание ошибки во время валидации
@@ -44,7 +43,7 @@ sample({
   clock: formSubmitted,
   filter: not($isValidEmail),
   fn: () => SignInPageError.InvalidEmail,
-  target: $error
+  target: $error,
 });
 
 // Отправка запроса на API если валидация прошла успешно
@@ -52,5 +51,5 @@ sample({
   clock: formSubmitted,
   source: $email,
   filter: $isValidEmail,
-  target: signInPost
+  target: signInPost,
 });
--- src/pages/auth-page/ui/sign-in-page/sign-in-page.tsx
+++ src/pages/auth-page/ui/sign-in-page/sign-in-page.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useCallback } from "react";
 import { useUnit } from "effector-react";
 import { useTranslation } from "react-i18next";
-import { ClipLoader } from 'react-spinners';
+import { ClipLoader } from "react-spinners";
 
 import { SInput } from "@/shared/ui/s-input";
 
@@ -9,27 +9,33 @@ import { SButton } from "@/shared/ui/s-button";
 import GoogleIcon from "../../assets/google-icon.svg?react";
 import { AuthPageLayout } from "../../layout/auth-page-layout";
 
-import { $email, $error, $pedding, $success, emailChanged, formSubmitted } from '../../model/auth';
+import { $email, $error, $pending, $success, emailChanged, formSubmitted } from "../../model/auth";
 
 import styles from "./sign-in-page.module.scss";
 
 export const SignInPage = () => {
   const { t } = useTranslation("auth-page");
 
-  const [email, pedding, error, success] = useUnit([$email, $pedding, $error, $success]);
+  const [email, pending, error] = useUnit([$email, $pending, $error, $success]);
   const [handleEmail, handleSubmitForm] = useUnit([emailChanged, formSubmitted]);
 
-  const onSubmitForm = useCallback((event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    handleSubmitForm();
-  }, [handleSubmitForm])
+  const onSubmitForm = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      handleSubmitForm();
+    },
+    [handleSubmitForm],
+  );
 
   const errorMessage = error && t(`errors.${error}`);
 
   return (
     <div className={styles.root}>
       <AuthPageLayout>
-        <form className={styles.inner} onSubmit={onSubmitForm}>
+        <form
+          className={styles.inner}
+          onSubmit={onSubmitForm}
+        >
           <h1 className={styles.title}>{t("sign-in-page.title")}</h1>
           <h2 className={styles.subtitle}>{t("sign-in-page.subtitle")}</h2>
 
@@ -39,16 +45,23 @@ export const SignInPage = () => {
             placeholder={t("sign-in-page.input-placeholder")}
             onChange={(event) => handleEmail(event.target.value)}
             className={styles.input}
-            disabled={pedding}
+            disabled={pending}
             errorMessage={errorMessage}
           />
 
           <SButton
             fullWidth
             className={styles.button}
-            disabled={pedding}
+            disabled={pending}
             type="submit"
-            leftIcon={pedding && <ClipLoader color="#fff" size={20} />}
+            leftIcon={
+              pending && (
+                <ClipLoader
+                  color="#fff"
+                  size={20}
+                />
+              )
+            }
           >
             {t("sign-in-page.get-started")}
           </SButton>
@@ -57,7 +70,7 @@ export const SignInPage = () => {
             leftIcon={<GoogleIcon />}
             variant={"outlined"}
             color={"secondary"}
-            disabled={pedding}
+            disabled={pending}
           >
             {t("sign-in-page.get-stated-with-google")}
           </SButton>
